333998b22617af450d8aa059253c47a8d4c35773
juan david rios <juandavid3196@gmail.com>
2026-09-21 17:48:40 -0500

    folders structure and primeNg setting added

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,28 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(
+      withInterceptors([
+        // authInterceptor,
+        // errorInterceptor,
+      ]),
+    ),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+      ripple: true,
+    }),
+  ],
 };
