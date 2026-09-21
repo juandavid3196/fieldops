85c17d3f92167ed9563f5f7a955e5932cf3bfc7c
juan david rios <juandavid3196@gmail.com>
2026-09-21 17:13:51 -0500

    Initial settings  into frontend and backend.

---
+++ backend/src/FieldOps.Application/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace FieldOps.Application;
+
+public class Class1
+{
+
+}
---
+++ backend/tests/FieldOps.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace FieldOps.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
